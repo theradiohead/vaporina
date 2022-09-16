d8bec33ec778c52fa049d0e0116661a7aa880368
theradiohead <ghassen.melliti@supcom.tn>
2022-09-16 09:49:23 +0100

    Step-3-Create-React-App

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a href="/">Vaporina</a>
+      </header>
+      <main>list products</main>
+    </div>
+  );
+}
+
+export default App;
